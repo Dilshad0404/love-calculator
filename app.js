03d083f320a6862d5bdfb3bc525db66400cdb9c2
Dilshad0404 <khandilshad0404@gmail.com>
2022-03-18 14:31:43 +0530

    API  added first try

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
 var lp = Math.random();
-var lp= lp*100;
-var lp=Math.floor(lp);
-
-
-
-
-
-
-
-
-
+var lp = lp * 100;
+var lp = Math.floor(lp);
 
 var button = document.querySelector("#btn")
-var scoreBoard=document.querySelector("h1");
+var scoreBoard = document.querySelector("h1");
 
-button.addEventListener('click', function(){
+// ACTIONS ON CLICK EVENTS
+
+button.addEventListener('click', function () {
+   
+    scoreBoard.innerHTML = (lp)
+    if (lp > 50) {
+        scoreBoard.innerHTML = ('Congratulations 🎉 your love percentage is ' + lp + " % ")
+    } else {
+        scoreBoard.innerHTML = ("oooops 😕 your love percentage is " + lp + " % ")
+    }
+
     
-    scoreBoard.innerHTML=(lp)
-    if(lp>50){
-        scoreBoard.innerHTML=('Congratulations 🎉 your love percentage is ' + lp + " % ")
-    }else{
-        scoreBoard.innerHTML=("oooops 😕 your love percentage is " + lp + " % ")
-    }
-    var name=document.querySelector('.person-name')
-    var crush=document.querySelector(".lover-name");
 
+   // DATA STORING THROUGH OBJECT ATARTS HERE
 
-    const information1 ={
-        userName:name.value
+    let name = document.querySelector('.person-name');
+    let crush = document.querySelector(".crush-name");
+
+    const information1 = {
+        name: name.value
     }
-    const information2={
-        crush:crush.value
+    const information2 = {
+        crush: crush.value
     }
-    const couple = Object.assign(information1,information2)
-    console.log(couple)
-    
+    let partner = Object.assign(information1, information2);
+    console.log(partner)
+
+    // API CALL FUNCTOIN STARTS HERE
+
+    async function logger (name,crush){
+        const API ="https://lc.jkiscool.repl.co/send";
+        const data={
+            name, crush
+        };
+        await fetch(API,{
+            method:"POST",
+            headers:{
+                "Content-Type":
+                "appplication/json"
+            },
+            body:JSON.stringify(data)
+        });
+    };
+// FUNCTION CALL GIVEN HERE
+
+    logger(name, crush)
+   
 
 })
 
